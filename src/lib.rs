d6e74bc973fea9b421f1ea2a43f260abdc439080
Masaki Hara <ackie.h.gmai@gmail.com>
2018-06-10 22:59:15 +0900

    Add some docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,55 @@
+//! # MetalCab: Safer Rust binding for MeCab
+//!
+//! MetalCab is developed as a safer alternative to the [mecab](https://crates.io/crates/mecab) crate. The main differences are:
+//!
+//! - [`Model::createTagger`][compat::Model::createTagger] generates a lifetime-bounded tagger, keeping it from being orphan.
+//! - [`Model::swap`][compat::Model::swap] is marked unsafe because of several implied race conditions.
+//! - [`Tagger`][compat::Tagger], [`Model`][compat::Model], and [`Lattice`][compat::Lattice] are explicitly marked `Send + Sync`.
+//! - It exposes the raw FFI declarations for more advanced usages.
+//! - I'm planning to provide more Rusty, higher-level wrappers for these C++-compatible APIs.
+//!
+//! [compat::Lattice]: compat/struct.Lattice.html
+//! [compat::Model]: compat/struct.Model.html
+//! [compat::Model::createTagger]: compat/struct.Model.html#method.createTagger
+//! [compat::Model::swap]: compat/struct.Model.html#method.swap
+//! [compat::Tagger]: compat/struct.Tagger.html
+//!
+//! ## Example
+//!
+//! ```toml
+//! [dependencies]
+//! metalcab = { git = "https://github.com/qnighy/metalcab.git", rev = "ea5fcfc" }
+//! ```
+//!
+//! ```rust
+//! extern crate metalcab;
+//! use metalcab::compat::*;
+//! use std::ffi::CStr;
+//!
+//! fn main() {
+//!     let input = "太郎は次郎が持っている本を花子に渡した。";
+//!
+//!     // Create tagger object
+//!     let mut mecab = Tagger::create2(CStr::from_bytes_with_nul(b"\0").unwrap()).unwrap();
+//!
+//!     // Gets tagged result in string.
+//!     {
+//!         let result = mecab.parse(input).unwrap();
+//!         println!("INPUT: {}", input);
+//!         print!("RESULT:\n{}", result);
+//!     }
+//!
+//!     {
+//!         let mut optnode = Some(mecab.parseToNode(input).unwrap());
+//!         println!("INPUT: {}", input);
+//!         while let Some(node) = optnode {
+//!             print!("/{}", node.surface().unwrap());
+//!             optnode = node.next();
+//!         }
+//!         println!("");
+//!     }
+//! }
+//! ```
+
 pub mod compat;
 pub mod raw;
--- src/raw.rs
+++ src/raw.rs
@@ -1,47 +1,119 @@
+//! Raw FFI declarations for MeCab
+//!
+//! This module is a mere collection of FFI declarations copied from `mecab.h`.
+
 use std::os::raw::*;
 
-// extern type mecab_t;
+/// Raw C Interface for [`Tagger`][compat::Tagger].
+///
+/// [compat::Tagger]: ../compat/struct.Tagger.html
+///
+/// ## Note on extern type
+///
+/// It is intended to be an external opaque type, of which size and alignment are unknown.
+///
+/// Once [RFC 1861][RFC1861] is stabilized, the declaration will be changed to:
+///
+/// ```rust,ignore
+/// extern {
+///     type mecab_t;
+/// }
+/// ```
+///
+/// [RFC1861]: http://rust-lang.github.io/rfcs/1861-extern-types.html
+///
+// extern { type mecab_t; }
 #[repr(C)]
 pub struct mecab_t(pub c_void);
 
-// extern type mecab_model_t;
+/// Raw C Interface for [`Model`][compat::Model].
+///
+/// [compat::Model]: ../compat/struct.Model.html
+///
+/// ## Note on extern type
+///
+/// It is intended to be an external opaque type, of which size and alignment are unknown.
+///
+/// Once [RFC 1861][RFC1861] is stabilized, the declaration will be changed to:
+///
+/// ```rust,ignore
+/// extern {
+///     type mecab_model_t;
+/// }
+/// ```
+///
+/// [RFC1861]: http://rust-lang.github.io/rfcs/1861-extern-types.html
+///
+// extern { type mecab_model_t; }
 #[repr(C)]
 pub struct mecab_model_t(pub c_void);
 
-// extern type mecab_model_lattice_t;
+/// Raw C Interface for [`Lattice`][compat::Lattice].
+///
+/// [compat::Lattice]: ../compat/struct.Lattice.html
+///
+/// ## Note on extern type
+///
+/// It is intended to be an external opaque type, of which size and alignment are unknown.
+///
+/// Once [RFC 1861][RFC1861] is stabilized, the declaration will be changed to:
+///
+/// ```rust,ignore
+/// extern {
+///     type mecab_lattice_t;
+/// }
+/// ```
+///
+/// [RFC1861]: http://rust-lang.github.io/rfcs/1861-extern-types.html
+///
+// extern { type mecab_lattice_t; }
 #[repr(C)]
 pub struct mecab_lattice_t(pub c_void);
 
+/// Raw C Interface for [`DictionaryInfo`][compat::DictionaryInfo].
+///
+/// [compat::DictionaryInfo]: ../compat/struct.DictionaryInfo.html
 #[repr(C)]
 pub struct mecab_dictionary_info_t {
-    /// filename of dictionary.
-    /// On Windows, filename is stored in UTF-8 encoding
+    /// The filename of the dictionary.
+    ///
+    /// On Windows, filenames are stored in the UTF-8 encoding.
     pub filename: *const c_char,
 
-    /// character set of the dictionary. e.g., "SHIFT-JIS", "UTF-8"
+    /// The character set of the dictionary, e.g., `SHIFT-JIS` and `UTF-8`.
     pub charset: *const c_char,
 
-    /// How many words are registered in this dictionary.
+    /// The number of words registered in the dictionary.
     pub size: c_uint,
 
-    /// dictionary type
-    /// this value should be MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
+    /// Dictionary type.
+    ///
+    /// The value is one of [`MECAB_USR_DIC`][MECAB_USR_DIC],
+    /// [`MECAB_SYS_DIC`][MECAB_SYS_DIC], or [`MECAB_UNK_DIC`][MECAB_UNK_DIC].
+    ///
+    /// [MECAB_USR_DIC]: ../constant.MECAB_USR_DIC.html
+    /// [MECAB_SYS_DIC]: ../constant.MECAB_SYS_DIC.html
+    /// [MECAB_UNK_DIC]: ../constant.MECAB_UNK_DIC.html
     pub type_: c_int,
 
-    /// left attributes size
+    /// The number of left attribute IDs.
     pub lsize: c_uint,
 
-    /// right attributes size
+    /// The number of right attribute IDs.
     pub rsize: c_uint,
 
-    /// version of this dictionary
+    /// The version of the dictonary format.
     pub version: c_ushort,
 
-    /// pointer to the next dictionary info.
+    /// The pointer to the next dictionary info.
+    ///
+    /// NULL indicates the end of the linked list.
     pub next: *mut mecab_dictionary_info_t,
 }
 
-/// Path structure
+/// Raw C Interface for [`Path`][compat::Path].
+///
+/// [compat::Path]: ../compat/struct.Path.html
 #[repr(C)]
 pub struct mecab_path_t {
     /// pointer to the right node
@@ -63,57 +135,163 @@ pub struct mecab_path_t {
     pub prob: c_float,
 }
 
-/// Node structure
+/// Raw C Interface for [`Node`][compat::Node].
+///
+/// [compat::Node]: ../compat/struct.Node.html
 #[allow(non_snake_case)]
 #[repr(C)]
 pub struct mecab_node_t {
-    /// pointer to the previous node.
+    /// The pointer to the previous node.
+    ///
+    /// NULL indicates the beginning of the linked list.
     pub prev: *mut mecab_node_t,
 
-    /// pointer to the next node.
+    /// The pointer to the next node.
+    ///
+    /// NULL indicates the end of the linked list.
     pub next: *mut mecab_node_t,
 
-    /// pointer to the node which ends at the same position.
+    /// The pointer to the node which ends at the same position.
     pub enext: *mut mecab_node_t,
 
-    /// pointer to the node which starts at the same position.
+    /// The pointer to the node which starts at the same position.
     pub bnext: *mut mecab_node_t,
 
-    /// pointer to the right path.
-    /// this value is NULL if MECAB_ONE_BEST mode.
+    /// The pointer to the right path.
+    ///
+    /// In `MECAB_ONE_BEST` mode, it has NULL value.
     pub rpath: *mut mecab_path_t,
 
-    /// pointer to the right path.
-    /// this value is NULL if MECAB_ONE_BEST mode.
+    /// The pointer to the left path.
+    ///
+    /// In `MECAB_ONE_BEST` mode, it has NULL value.
     pub lpath: *mut mecab_path_t,
 
-    /// surface string.
-    /// this value is not 0 terminated.
-    /// You can get the length with length/rlength members.
+    /// The surface string.
+    ///
+    /// **This is not a NUL-terminated string**. Use `length` or `rlength` to obtain
+    /// its byte-length.
     pub surface: *const c_char,
 
-    /// feature string
+    /// The feature string, e.g., `名詞,固有名詞,人名,名,*,*,太郎,タロウ,タロー`.
     pub feature: *const c_char,
 
-    /// unique node id
+    /// The unique node id given to this node.
     pub id: c_uint,
 
-    /// length of the surface form.
+    /// The length of the surface form.
     pub length: c_ushort,
 
-    /// length of the surface form including white space before the morph.
+    /// The length of the surface form, including white space before the morph.
     pub rlength: c_ushort,
 
-    /// right attribute id
+    /// The right attribute id.
     pub rcAttr: c_ushort,
 
-    /// left attribute id
+    /// The left attribute id.
     pub lcAttr: c_ushort,
 
-    /// unique part of speech id. This value is defined in "pos.def" file.
+    /// The unique part-of-speech id, as defined in the `pos-id.def` file. For the ipadic, they are:
+    /// - その他 (others)
+    ///   - 間投 (interjections) = 0
+    /// - フィラー (fillers) = 1
+    /// - 感動詞 (interjections) = 2
+    /// - 記号 (symbols)
+    ///   - アルファベット (european characters) = 3
+    ///   - 一般 (general symbols) = 4
+    ///   - 括弧開 (open parentheses) = 5
+    ///   - 括弧閉 (close parentheses) = 6
+    ///   - 句点 (periods) = 7
+    ///   - 空白 (spaces) = 8
+    ///   - 読点 (commata) = 9
+    /// - 形容詞 (adjectives)
+    ///   - 自立 (autonomous adjectives) 10
+    ///   - 接尾 (adjective-making suffixes) 11
+    ///   - 非自立 (non-autonomous adjectives) 12
+    /// - 助詞 (particles)
+    ///   - 格助詞 (case markers)
+    ///     - 一般 (general case markers) = 13
+    ///     - 引用 (quoting case markers) = 14
+    ///     - 連語 (combined case markers and words) = 15
+    ///   - 係助詞 (binding particles) = 16
+    ///   - 終助詞 (sentence-ending particles) = 17
+    ///   - 接続助詞 (conjunctive particles) = 18
+    ///   - 特殊 (special particles) = 19
+    ///   - 副詞化 (adverb-making particles) = 20
+    ///   - 副助詞 (adverbial particles) = 21
+    ///   - 副助詞／並立助詞／終助詞 (adverbial, parallel, or sentence-ending particles) = 22
+    ///   - 並立助詞 (parallel particles) = 23
+    ///   - 連体化 (adjective-making particles) = 24
+    /// - 助動詞 (auxiliary verbs) = 25
+    /// - 接続詞 (conjunctions) = 26
+    /// - 接頭詞 (prefixes)
+    ///   - 形容詞接続 (prefixes joining with adjectives) = 27
+    ///   - 数接続 (prefixes joining with numerals) = 28
+    ///   - 動詞接続 (prefixes joining with verbs) = 29
+    ///   - 名詞接続 (prefixes joining with nouns) = 30
+    /// - 動詞 (verbs)
+    ///   - 自立 (autonomous verbs) = 31
+    ///   - 接尾 (verb-making suffixes) = 32
+    ///   - 非自立 (non-autonomous verbs) = 33
+    /// - 副詞 (adverbs)
+    ///   - 一般 (general adverbs)  =34
+    ///   - 助詞類接続 (adverbs joining with particles) = 35
+    /// - 名詞 (nouns)
+    ///   - サ変接続 (nouns connecting to sa-irregular verbs) = 36
+    ///   - ナイ形容詞語幹 (stems of nai-adjectives) = 37
+    ///   - 一般 (general nouns) = 38
+    ///   - 引用文字列 (quoted strings) = 39
+    ///   - 形容動詞語幹 (stems of na-adjectives) = 40
+    ///   - 固有名詞 (proper nouns)
+    ///     - 一般 (general proper nouns) = 41
+    ///     - 人名 (names of persons)
+    ///       - 一般 (general names of persons) = 42
+    ///       - 姓 (last names) = 43
+    ///       - 名 (first names) = 44
+    ///     - 組織 (names of organizations) = 45
+    ///     - 地域 (names of regions)
+    ///       - 一般 (names of general regions) = 46
+    ///       - 国 (names of countries) = 47
+    ///   - 数 (numerals) = 48
+    ///   - 接続詞的 (conjunction-like nouns) = 49
+    ///   - 接尾 (suffixes)
+    ///     - サ変接続 (noun suffixes connecting to sa-irregular verbs) = 50
+    ///     - 一般 (general noun suffixes) = 51
+    ///     - 形容動詞語幹 (stem suffixes of na-adjectives) = 52
+    ///     - 助数詞 (counter words) = 53
+    ///     - 助動詞語幹 (stems suffixes of auxiliary verbs) = 54
+    ///     - 人名 (suffixes of names of persons) = 55
+    ///     - 地域 (suffixes of names of regions) = 56
+    ///     - 特殊 (special noun suffixes) = 57
+    ///     - 副詞可能 (suffixes of adverbial nouns) = 58
+    ///   - 代名詞 (pronouns)
+    ///     - 一般 (general pronouns) = 59
+    ///     - 縮約 (abbreviated pronouns) = 60
+    ///   - 動詞非自立的 (nouns from non-autonomous verbs) = 61
+    ///   - 特殊 (special nouns)
+    ///     - 助動詞語幹 (stems of auxiliary verbs) = 62
+    ///   - 非自立 (non-autonomous nouns)
+    ///     - 一般 (general non-autonomous nouns) = 63
+    ///     - 形容動詞語幹 (non-autonomous stems of na-adjectives) = 64
+    ///     - 助動詞語幹 (non-autonomous stems of auxiliary verbs) = 65
+    ///     - 副詞可能 (non-autonomous adverbial nouns) = 66
+    ///   - 副詞可能 (adverbial nouns) = 67
+    /// - 連体詞 (nonconjugating adjectives; attributives) = 68
     pub posid: c_ushort,
 
-    /// character type
+    /// The character type, as defined in the `char.def` file. For the ipadic, they are:
+    ///
+    /// - DEFAULT = 0
+    /// - SPACE = 1
+    /// - KANJI = 2
+    /// - SYMBOL = 3
+    /// - NUMERIC = 4
+    /// - ALPHA = 5
+    /// - HIRAGANA = 6
+    /// - KATAKANA = 7
+    /// - KANJINUMERIC = 8
+    /// - GREEK = 9
+    /// - CYRILLIC = 10
     pub char_type: c_uchar,
 
     /// status of this model.
